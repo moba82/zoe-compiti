6585455f1779da45039a81a88161b58c8f235290
moba82 <bandieram@gmail.com>
2026-03-12 21:21:21 +0100

    Delete zoe-compiti OK/src/supabase.js

--- zoe-compiti OK/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = "https://elpvsadyzdwienilfjf.supabase.co"
-const SUPABASE_KEY = "sb_publishable_lQhB9u7ESFqw1mRQyjP0GA_-RKMNcsB"
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
