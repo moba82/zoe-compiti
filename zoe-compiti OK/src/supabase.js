3b221c79fa911945a50f71e533a26a22ee6738de
moba82 <bandieram@gmail.com>
2026-03-12 20:46:18 +0100

    Update supabase.js

--- zoe-compiti OK/src/supabase.js
+++ zoe-compiti OK/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = "https://elpvsadyzdwienilfjf.supabase.co"
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVscHZzYWR5emR3dmllbmlsZmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMDE3NDksImV4cCI6MjA4ODg3Nzc0OX0.VAa1FNRp87xxzukoxc0oE8vzj_ZJ_N0mifLe7n4dLSI"
+const SUPABASE_KEY = "sb_publishable_lQhB9u7ESFqw1mRQyjP0GA_-RKMNcsB"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
